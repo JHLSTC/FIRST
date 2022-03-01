1da523cd9cde3a2a177fbfbef9f69cc2de2d83b2
JHLSTC <97880101+JHLSTC@users.noreply.github.com>
2022-03-01 20:43:03 +0800

    Add files via upload

--- 3.c
+++ 3.c
@@ -1,49 +1,17 @@
 #include<stdio.h>
-#include<string.h>
-//extern int AB;申明变量。
-//const//常变量。
-//enum{};枚举
-int Add(int x,int y,int z)
+struct print
 {
-    int m =0;
-    m = x + y;
-    return m;
-}
-struct man
-{
-    char name[20];
-    int age;
-    char id[30];
-
+    int c ;
+    int b;
 };
-void  swap(int* x,int* y)
-{
-    int z = *x ;
-    *x = *y;
-    *y = z;
 
-
-
-}
 int main()
 {
-    int a;
-    int b;
-    int* c = &a;
-    int* d = &b;
- //struct man s ={"张三",20 ,203320287};
-   //printf("%s %d %d",s.name,s.age,s.id);
 
-  //struct man*i =&s;
-  //printf("%s %d %d",(*i).name,(*i).age,(*i).id);
-  //printf("%s %d %d",i->name,i->id,i->age);
-  a = 10;
-  b = 20;
 
-  swap(c,d);
-  printf(" a = %d   b = %d",a ,b);
 
-    return  0;
+   struct print a={12,10};
+   printf("%d",a.b);
+
+    return 0;
 }
-
-
