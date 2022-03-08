ad434ace92727a6d1fab15a13dd810fa285dbc4a
JHLSTC <97880101+JHLSTC@users.noreply.github.com>
2022-03-08 17:44:00 +0800

    Add files via upload

---
+++ test_practise.c
@@ -0,0 +1,62 @@
+//#include<string.h>
+//#include<stdio.h>
+//#include<assert.h>
+//char* my_strcpy(char* need, char* s)
+//{
+//	char* a = need;
+//	int i = 0;
+//	assert(need != NULL);
+//	assert(s != NULL);
+//	while (*need++ = *s++)
+//	{
+//		;
+//	}
+//	return a;
+//}
+//int main()
+//{
+//	char a[] = "100000000";
+//	char b[] = "abcd";
+//	printf("%s", my_strcpy(a, b));
+//
+//
+//
+//
+//
+//	return 0;
+//}
+//#include<stdio.h>
+
+//健壮性
+//size_t my_strlen(char str[])
+//{
+//	int i = 0;
+//	while(str[i] !='\0')
+//	{
+//		i++;
+//	}
+//	return i;
+//}
+//int main()
+//{
+//	char a[] = "1abcdefgh";
+//	printf("%u", my_strlen(a));
+//	return 0;
+//}
+// //问题：
+//数列出首项外都没有地址与数列名想关联；
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
