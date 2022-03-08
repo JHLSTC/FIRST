67549a5fbce128cef4de979b3a50046654686750
JHLSTC <97880101+JHLSTC@users.noreply.github.com>
2022-03-08 21:06:54 +0800

    Add files via upload

--- test_practise.c
+++ test_practise.c
@@ -45,10 +45,23 @@
 //}
 // //问题：
 //数列出首项外都没有地址与数列名想关联；
-
- 
-
-
+#include<stdio.h>
+//int main()
+//{
+//    int s_canf;
+//    int out;
+//    scanf_s("%d %d",&s_canf,&out);
+//    int test = s_canf ^ out;
+//    int count = 0;
+//    while (test)
+//    {
+//        test = test & (test - 1);
+//        count++;
+//    }
+//    printf("二进制中位数不同的为%d", count);
+//
+//    return 0;
+//}
 
 
 
