453f666e32fb793df4d11ae59e38c715284f53ca
JHLSTC <97880101+JHLSTC@users.noreply.github.com>
2022-03-17 09:33:31 +0800

    Add files via upload

---
+++ 习题2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf_s("%d", &a);
+//	while (a)
+//	{
+//		int b = 0;
+//		b = a % 10;
+//		a /= 10;
+//		printf("%d", b);
+//
+//	}
+//
+//	return 0;
+//}
